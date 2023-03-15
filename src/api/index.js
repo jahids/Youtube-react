3450c248891fd4021cf55465b01703cdecd1826d
unknown <evanjahid321@gmail.com>
2023-03-15 19:11:00 +0600

    allignment change

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,15 @@
 import axios from "axios";
 
 const key = `AIzaSyAd4HCev6UKhhxXw0sN3fibYmohoeN1yyg`;
-// const key = ;
-// &nextPageToken=EAAaBlBUOkNESQ
-// console.log(import.meta.env.LKJDAL, "kk")
-
-// const key = import.meta.env.VITE_API_KEY;
-
 const getPlaylist = async (playlistID, pageToken = '', result = []) => {
-	const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=${playlistID}&key=${key}&pageToken=${pageToken}`;
+    const URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=${playlistID}&key=${key}&pageToken=${pageToken}`;
 
-	const { data } = await axios.get(URL);
-	result = [...result, ...data.items];
-	if (data.nextPageToken) {
-		result = getPlaylist(playlistID, data.nextPageToken, result);
-	}
-	return result;
+    const { data } = await axios.get(URL);
+    result = [...result, ...data.items];
+    if (data.nextPageToken) {
+        result = getPlaylist(playlistID, data.nextPageToken, result);
+    }
+    return result;
 };
 
 export default getPlaylist;
